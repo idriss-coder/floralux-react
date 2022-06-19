c45cf8bff604717f0b02f032c9c73b29c62a7524
IDRISSHACKER <idrisscoder@gmail.com>
2022-06-19 18:56:16 +0100

    Create essentials folder

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable import/extensions */
+import React from "react";
+import { render, screen } from "@testing-library/react";
+// eslint-disable-next-line import/no-unresolved
+import App from "./App";
 
-test('renders learn react link', () => {
+// eslint-disable-next-line no-undef
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  // eslint-disable-next-line no-undef
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {process.env.NODE_ENV}
         </a>
       </header>
     </div>
