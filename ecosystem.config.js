ba4b828f75802da5b3a99b6ab84aed5496748ed6
IDRISSHACKER <idrisscoder@gmail.com>
2022-07-07 23:08:13 +0100

    Modify ecosystem setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  apps : [{
-    script: 'bin/www',
-    watch: '.'
-  }],
-
-  deploy : {
-    production : {
-      user: 'aysfishh117',
-      host: '78.46.11.32',
-      ref  : 'origin/master',
-      repo: 'https://github.com/IDRISSHACKER/floraluxapi.git',
-      path: '/var/www/floralux.guihon.cm',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  deploy: {
+    production: {
+      user: "aysfishh117",
+      host: "78.46.11.32",
+      ref: "origin/master",
+      repo: "https://github.com/IDRISSHACKER/floraluxapi.git",
+      path: "/var/www/floralux.guihon.cm",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
 };
